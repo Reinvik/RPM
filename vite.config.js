27eb95c199b9f4ee346a4aa184381a025e14acd8
Ariel Mellado <ariel@smartlean.cl>
2026-06-22 16:36:28 -0400

    fix: desactivar autoUpdate agresivo de la PWA para solucionar las recargas automaticas de pagina

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       includeAssets: ['favicon.png', 'pwa-192x192.png', 'pwa-512x512.png'],
       manifest: {
         name: 'Nexus RPM - Planificación de Recursos',
